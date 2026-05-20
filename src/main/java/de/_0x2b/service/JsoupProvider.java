4f6557567884eb10ae98390b974917ee9220412e
Felix Zimmermann <felix.zimmermann.1.de+github@gmail.com>
2026-05-20 18:00:36 +0200

    fix (backend): custom user agent for accessing feeds

--- src/main/java/de/_0x2b/service/JsoupProvider.java
+++ src/main/java/de/_0x2b/service/JsoupProvider.java
@@ -3,6 +3,7 @@ package de._0x2b.service;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import java.io.IOException;
 
 /**
@@ -10,6 +11,13 @@ import java.io.IOException;
  */
 public class JsoupProvider {
     public Connection.Response execute(String url) throws IOException {
-        return Jsoup.connect(url).followRedirects(true).ignoreContentType(true).execute();
+        var config = ConfigProvider.getConfig();
+        var userAgent = config.getValue("app.http.user-agent", String.class);
+
+        return Jsoup.connect(url)
+                .followRedirects(true)
+                .ignoreContentType(true)
+                .userAgent(userAgent)
+                .execute();
     }
 }
